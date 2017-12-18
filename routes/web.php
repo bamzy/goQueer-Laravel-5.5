8f474d106962519ca90489c27de5fc5c86598e87
Bamdad <bamdad.ag@gmail.com>
2017-12-18 00:30:18 -0700

    some routes have been moved

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,33 @@ Route::get('/', function () {
 Route::get('/', 'HomeController@index')->name('simple');
 Route::get('/ajax', 'HomeController@ajax')->name('ajax');
 
-//Route::get('/login', 'HomeController@showLogin')->name('login');
+Route::get('/login', 'HomeController@showLogin')->name('login');
 //Route::post('/login', 'HomeController@doLogin')->name('login');
 //Route::get('/logout', 'HomeController@doLogout')->name('logout');
 
 
+Route::get('/documentation', function () {
+    return view('document.index')->with('email',Auth::user()->email);
+});
+
+Route::get('/features', function () {
+    return view('feature.index')->with('email',Auth::user()->email);
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::resource('order', 'OrderController');
+Route::resource('location','LocationController');
+Route::resource('draft','DraftController');
+Route::resource('media','MediaController');
+Route::resource('gallery','GalleryController');
+Route::resource('message','MessageController');
+Route::resource('gallery_media','GalleryMediaController');
+Route::resource('test','TestController');
+Route::resource('final','FinalController');
+Route::resource('map','MapController');
+Route::resource('set','SetController');
+Route::resource('profile','ProfileController');
+Route::resource('hint','HintController');
+Route::auth();
